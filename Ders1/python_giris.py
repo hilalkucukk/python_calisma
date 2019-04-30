79e5d421981da77deff099c44b8f45f0bd4ceedf
Hilal Küçük <39016068+hilalkucuk25@users.noreply.github.com>
2019-05-01 00:18:37 +0300

    ilk ders eklendi

---
+++ Ders1/python_giris.py
@@ -0,0 +1,29 @@
+print('merhaba dünya')
+# Bu tekli yorum satırı
+
+"""
+Bu çoklu yorum satırı
+"""
+
+# Değişken atama ve ekrana yazma
+
+a = 3
+b = 3.2
+c, d = 5, 6
+
+print("a: ", a)
+print("b: ", b)
+print("c: ", c)
+print("d: ", d)
+
+print("a'nın değeri {}, b'nin değeri {},c'nin değeri {},d'nin değeri {}'tir.".format(a, b, c, d))
+
+# Klavyeden veri alma ve tip dönüşümleri
+
+sayi1=input('Birinci sayıyı giriniz:')
+sayi2=int(input('İkinci sayıyı giriniz:'))
+sayi1=int(sayi1)
+
+toplam=sayi1+sayi2
+print('Toplam:', toplam)
+
