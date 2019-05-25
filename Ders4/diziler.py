4dee9a7a716c556e579d60d23abac06b6877ad08
Hilal Küçük <39016068+hilalkucukk@users.noreply.github.com>
2019-05-26 01:56:05 +0300

    Diziler

---
+++ Ders4/diziler.py
@@ -0,0 +1,13 @@
+dizi1 = [1, 2, 3, 4]
+dizi2 = ['a', 'b', 'c', 'd']
+dizi3 = [1, 'a', 'b', 123]
+
+print(dizi1)
+print(dizi2)
+print(dizi3)
+
+for i in dizi1:
+    print(i)
+
+for i in range(len(dizi1)):
+    print(dizi1[i])
