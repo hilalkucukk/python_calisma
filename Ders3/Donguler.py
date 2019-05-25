70ba872fd13a9ff77ddf367efbdccc271645f023
Hilal Küçük <39016068+hilalkucukk@users.noreply.github.com>
2019-05-26 01:38:07 +0300

    For, While Konuları

---
+++ Ders3/Donguler.py
@@ -0,0 +1,28 @@
+# sayi = 0
+# while sayi < 10:
+#     print('Sayımın değeri: ', sayi)
+#     sayi = sayi + 1
+
+
+# sayac = 0
+# toplam = 0
+# while sayac < 5:
+#     sayi = int(input('Bir sayi giriniz: '))
+#     toplam = toplam + sayi;
+#     sayac = sayac + 1
+#
+# print('Sonuç: ', toplam)
+
+
+# isim='Hilal Küçük'
+# for karakter in isim:
+#     print('Karakter: ', karakter)
+
+# isim='Hilal Küçük'
+# for i in range(5,10):
+#     print(i-4,isim)
+
+
+for i in range(10,1,-2):
+    print(i)
+
